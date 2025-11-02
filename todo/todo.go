49c6237d789f77b19715091c53d4e191b5294ac1
Brett Vonbokel <vonbbr@gmail.com>
2025-11-02 12:43:56 -0600

    modifications to todo.go

--- todo/todo.go
+++ todo/todo.go
@@ -1,5 +1,51 @@
 package todo
 
+import (
+	"fmt"
+	"os"
+	"encoding/json"
+)
+
 type Item struct {
 	Text string
+	Priority int
+}
+
+func SaveItems(filename string, items []Item) error {
+	b, err := json.Marshal(items)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(b))
+	err = os.WriteFile(filename, b, 0644)
+	if err != nil {
+		fmt.Println("%v", err)
+		return err
+	}
+
+	return nil
+}
+
+func ReadItems(filename string) ([]Item, error) {
+	b, err := os.ReadFile(filename)
+	if err != nil {
+		return []Item{}, err
+	}
+	var items []Item
+	if err := json.Unmarshal(b, &items); err != nil {
+		return []Item{}, err
+	}
+
+	return items, nil
+}
+
+func (i *Item) SetPriority(pri int) {
+	switch pri {
+	case 1:
+		i.Priority = 1
+	case 3:
+		i.Priority = 3
+	default:
+		i.Priority = 2
+	}
 }
